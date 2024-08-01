e2bc5fdea62e318f11959552f1233825fc406931
mposolda <mposolda@gmail.com>
2024-08-01 09:39:14 +0200

    JEE mod tests working - including classpath test
    Signed-off-by: mposolda <mposolda@gmail.com>

---
+++ testsuite/admin-client-jee-tests/src/test/java/org/keycloak/client/testsuite/adminclientjee/RealmsJEETest.java
@@ -0,0 +1,9 @@
+package org.keycloak.client.testsuite.adminclientjee;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author <a href="mailto:mposolda@redhat.com">Marek Posolda</a>
+ */
+public class RealmsJEETest extends org.keycloak.client.testsuite.adminclient.RealmsTest {
+}
